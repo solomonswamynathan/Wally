e7a0f1c7ab6e57a783c8e856216e5a6fed2b5779
Solomon S <solomonswamynathan17@gmail.com>
2024-02-19 09:11:32 +0530

    Updated views to bindings

--- app/src/main/java/com/example/wally/MainActivity.kt
+++ app/src/main/java/com/example/wally/MainActivity.kt
@@ -3,26 +3,27 @@ package com.example.wally
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import androidx.core.content.ContextCompat
+import com.example.wally.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
         window.statusBarColor = ContextCompat.getColor(this, R.color.black)
 
 
         //screen navigation using Intent
-        var loginButton = findViewById<Button>(R.id.button_login)
-        loginButton.setOnClickListener {
+        binding.buttonLogin.setOnClickListener {
             val intent= Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        var registerButton = findViewById<Button>(R.id.register_button)
-        registerButton.setOnClickListener {
+        binding.registerButton.setOnClickListener {
             val intent= Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
