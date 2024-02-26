5eef5897d28358909f2acaaedf5a1acea3f4899c
Solomon S <solomonswamynathan17@gmail.com>
2024-02-26 09:14:30 +0530

    Modified file to have file fetched from raw

--- app/src/main/java/com/example/wally/HomeActivity.kt
+++ app/src/main/java/com/example/wally/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.wally
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.View
@@ -9,15 +12,18 @@ import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.bumptech.glide.Glide
 import com.example.wally.databinding.ActivityHomeBinding
 import com.google.gson.Gson
 import okhttp3.*
 import org.json.JSONException
 import java.io.IOException
+import java.io.InputStream
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var photoAdapter: PhotoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -25,39 +31,79 @@ class HomeActivity : AppCompatActivity() {
         supportActionBar?.hide()
         window.statusBarColor = ContextCompat.getColor(this, R.color.black)
 
-        val client = OkHttpClient()
-        val url = "https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=30"
-        val request = Request.Builder().url(url).build()
+        val jsonString = readJsonFile(this, R.raw.data)
+        val photoList = parseJsonToPhotoList(jsonString)
 
-        client.newCall(request).enqueue(object: Callback{
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
+        photoAdapter = PhotoAdapter(photoList)
+        val layoutManager = LinearLayoutManager(this@HomeActivity)
+        binding.photoRecyclerView.layoutManager = layoutManager
+        binding.photoRecyclerView.adapter = photoAdapter
 
-            override fun onResponse(call: Call, response: Response) {
+        photoAdapter.onItemClick = {
+            Log.i("HomeActivity: ", "Clicked on image")
+            val intent = Intent(this@HomeActivity, PhotoActivity::class.java)
+            intent.putExtra("photo", it)
+            startActivity(intent)
+        }
 
-                if(response.isSuccessful){
-                    runOnUiThread {
-                        try{
-                            val jsonString= response.body?.string()
-                            val gson= Gson()
-                            val photoResponse: Wallpapers= gson.fromJson(jsonString, Wallpapers::class.java)
-
-
-                            //
-                            val layoutManager = LinearLayoutManager(this@HomeActivity)
-                            binding.photoRecyclerView.layoutManager = layoutManager
-                            binding.photoRecyclerView.adapter = PhotoAdapter(photoResponse.photos)
-                        }catch (e:JSONException){
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            }
-        })
+//        val client = OkHttpClient()
+//        val url = "https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=30"
+//        val request = Request.Builder().url(url).build()
+//
+//        client.newCall(request).enqueue(object: Callback{
+//            override fun onFailure(call: Call, e: IOException) {
+//                e.printStackTrace()
+//            }
+//
+//            override fun onResponse(call: Call, response: Response) {
+//                if(response.isSuccessful){
+//                    runOnUiThread {
+//                        try{
+//                            val jsonString= response.body?.string()
+//                            val gson= Gson()
+//                            val photoResponse: Wallpapers= gson.fromJson(jsonString, Wallpapers::class.java)
+//
+//                            val photoAdapter = PhotoAdapter(photoResponse.photos)
+//                            val layoutManager = LinearLayoutManager(this@HomeActivity)
+//                            binding.photoRecyclerView.layoutManager = layoutManager
+//                            binding.photoRecyclerView.adapter = photoAdapter
+//
+//                            photoAdapter.onItemClick = {
+//                                Log.i("HomeActivity: ","Clicked on image")
+//                                val intent = Intent(this@HomeActivity, PhotoActivity::class.java)
+//                                intent.putExtra("photo", it)
+//                                startActivity(intent)
+//                            }
+//
+//                        }catch (e:JSONException){
+//                            e.printStackTrace()
+//                        }
+//                    }
+//                }
+//            }
+//        })
     }
+
+    private fun readJsonFile(context: Context, resourceId: Int): String{
+        val inputStream: InputStream = context.resources.openRawResource(resourceId)
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        return String(buffer,Charsets.UTF_8)
+    }
+
+    private fun parseJsonToPhotoList(jsonString: String):List<Photo> {
+        val gson = Gson()
+        val photoResponse: Wallpapers = gson.fromJson(jsonString, Wallpapers::class.java)
+        return photoResponse.photos
+    }
+
+
     class PhotoAdapter(private val photo: List<Photo>):
         RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>(){
+
+        var onItemClick: ((Photo) -> Unit)? = null
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
             val itemView= LayoutInflater.from(parent.context)
@@ -73,6 +119,10 @@ class HomeActivity : AppCompatActivity() {
                 .load(photo.url)
                 .placeholder(R.drawable.ic_baseline_image_24)
                 .into(holder.photoImageView)
+
+            holder.itemView.setOnClickListener {
+                onItemClick?.invoke(photo)
+            }
         }
 
         override fun getItemCount(): Int {
