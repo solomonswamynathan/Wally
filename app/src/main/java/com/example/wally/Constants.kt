0e5c1b458f03c7c3ea49686f8f68dd3c2d556ebd
Solomon S <solomonswamynathan17@gmail.com>
2024-03-03 12:53:01 +0530

    Constants for CameraActivity.kt

---
+++ app/src/main/java/com/example/wally/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.wally
+
+import android.Manifest
+
+object Constants {
+    const val TAG= "cameraX"
+    const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
+    const val REQUEST_CODE_PERMISSION = 123
+    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+}
